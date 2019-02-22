13d2b78252c865aae6af7995e8453960eb4546e3
chris ricketts <chris.ricketts@steribar.com>
2019-02-22 10:46:34 +0000

    Fix setting canadian french layout in testing

--- tests/all_layouts.rs
+++ tests/all_layouts.rs
@@ -21,7 +21,7 @@ const RDESC: [u8; 63] = [
 
 const ALPHA_NUMERIC: &'static str =
     "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const ESCAPED_SYMBOLS: &'static str = "\\#\\!\\$\\%\\&\\'\\(\\)\\*\\+\\,\\-\\.\\/\\:\\;\\<\\=\\>\\?\\@\\\\[\\]\\^\\_\\`\\{\\|\\}\\~\"";
+const SYMBOLS: &'static str = "\"#!$%&'()*+,-.\\/:;<=>?@[]^_`{|}~\"";
 //const EXPECTED_SYMBOLS: &'static str = "#!$%&'()*+,-./:;<=>?@\\[]^_`{|}~\"";
 
 lazy_static! {
@@ -36,7 +36,7 @@ lazy_static! {
         "LAYOUT_IRISH" => ("ie", None),
         "LAYOUT_SWEDISH" => ("se", None),
         "LAYOUT_GERMAN_SWISS" => ("ch", None),
-        "LAYOUT_CANADIAN_FRENCH" => ("cf", None),
+        "LAYOUT_CANADIAN_FRENCH" => ("ca", Some("fr")),
         "LAYOUT_SPANISH" => ("es", None),
         "LAYOUT_PORTUGUESE" => ("pt", None),
         "LAYOUT_ICELANDIC" => ("is", None),
@@ -198,11 +198,7 @@ test_layout!(
     LAYOUT_FRENCH_BELGIAN,
     ALPHA_NUMERIC
 );
-test_layout!(
-    test_alphanumeric_layout_irish,
-    LAYOUT_IRISH,
-    ALPHA_NUMERIC
-);
+test_layout!(test_alphanumeric_layout_irish, LAYOUT_IRISH, ALPHA_NUMERIC);
 test_layout!(
     test_alphanumeric_layout_swedish,
     LAYOUT_SWEDISH,
@@ -278,118 +274,62 @@ test_layout!(
     LAYOUT_UNITED_KINGDOM,
     ALPHA_NUMERIC
 );
-test_layout!(
-    test_symbols_layout_german,
-    LAYOUT_GERMAN,
-    ESCAPED_SYMBOLS
-);
+test_layout!(test_symbols_layout_german, LAYOUT_GERMAN, SYMBOLS);
 test_layout!(
     test_symbols_layout_portuguese_brazilian,
     LAYOUT_PORTUGUESE_BRAZILIAN,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
-test_layout!(
-    test_symbols_layout_french,
-    LAYOUT_FRENCH,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_us_english,
-    LAYOUT_US_ENGLISH,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_finnish,
-    LAYOUT_FINNISH,
-    ESCAPED_SYMBOLS
-);
+test_layout!(test_symbols_layout_french, LAYOUT_FRENCH, SYMBOLS);
+test_layout!(test_symbols_layout_us_english, LAYOUT_US_ENGLISH, SYMBOLS);
+test_layout!(test_symbols_layout_finnish, LAYOUT_FINNISH, SYMBOLS);
 test_layout!(
     test_symbols_layout_spanish_latin_america,
     LAYOUT_SPANISH_LATIN_AMERICA,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
 test_layout!(
     test_symbols_layout_french_belgian,
     LAYOUT_FRENCH_BELGIAN,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
-test_layout!(
-    test_symbols_layout_irish,
-    LAYOUT_IRISH,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_swedish,
-    LAYOUT_SWEDISH,
-    ESCAPED_SYMBOLS
-);
+test_layout!(test_symbols_layout_irish, LAYOUT_IRISH, SYMBOLS);
+test_layout!(test_symbols_layout_swedish, LAYOUT_SWEDISH, SYMBOLS);
 test_layout!(
     test_symbols_layout_german_swiss,
     LAYOUT_GERMAN_SWISS,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
 test_layout!(
     test_symbols_layout_canadian_french,
     LAYOUT_CANADIAN_FRENCH,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
-test_layout!(
-    test_symbols_layout_spanish,
-    LAYOUT_SPANISH,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_portuguese,
-    LAYOUT_PORTUGUESE,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_icelandic,
-    LAYOUT_ICELANDIC,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_turkish,
-    LAYOUT_TURKISH,
-    ESCAPED_SYMBOLS
-);
+test_layout!(test_symbols_layout_spanish, LAYOUT_SPANISH, SYMBOLS);
+test_layout!(test_symbols_layout_portuguese, LAYOUT_PORTUGUESE, SYMBOLS);
+test_layout!(test_symbols_layout_icelandic, LAYOUT_ICELANDIC, SYMBOLS);
+test_layout!(test_symbols_layout_turkish, LAYOUT_TURKISH, SYMBOLS);
 test_layout!(
     test_symbols_layout_us_international,
     LAYOUT_US_INTERNATIONAL,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
 test_layout!(
     test_symbols_layout_canadian_multilingual,
     LAYOUT_CANADIAN_MULTILINGUAL,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
 test_layout!(
     test_symbols_layout_french_swiss,
     LAYOUT_FRENCH_SWISS,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
-test_layout!(
-    test_symbols_layout_danish,
-    LAYOUT_DANISH,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_italian,
-    LAYOUT_ITALIAN,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_german_mac,
-    LAYOUT_GERMAN_MAC,
-    ESCAPED_SYMBOLS
-);
-test_layout!(
-    test_symbols_layout_norwegian,
-    LAYOUT_NORWEGIAN,
-    ESCAPED_SYMBOLS
-);
+test_layout!(test_symbols_layout_danish, LAYOUT_DANISH, SYMBOLS);
+test_layout!(test_symbols_layout_italian, LAYOUT_ITALIAN, SYMBOLS);
+test_layout!(test_symbols_layout_german_mac, LAYOUT_GERMAN_MAC, SYMBOLS);
+test_layout!(test_symbols_layout_norwegian, LAYOUT_NORWEGIAN, SYMBOLS);
 test_layout!(
     test_symbols_layout_united_kingdom,
     LAYOUT_UNITED_KINGDOM,
-    ESCAPED_SYMBOLS
+    SYMBOLS
 );
